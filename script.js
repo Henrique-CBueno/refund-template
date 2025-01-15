db303985ace9cdee9b7f55b9f499cb62e5f41eff
Henrique-CBueno <henriquecb2507@outlook.pt>
2025-01-15 11:35:42 -0300

    Add currency formatting and expense submission functionality

---
+++ script.js
@@ -0,0 +1,57 @@
+const amount = document.getElementById("amount")
+const expense = document.getElementById("expense")
+const category = document.getElementById("category")
+const form = document.querySelector("form")
+
+
+
+
+
+form.onsubmit = (event)=>{
+    event.preventDefault()
+
+
+
+    const newExpense ={
+        id: new Date().getTime(),
+        expense: expense.value,
+        category_id: category.value,
+        category_name: category.options[category.selectedIndex].text,
+        amount: amount.value,
+        currate_at: new Date(),
+    }
+
+
+
+    console.log(newExpense)
+    amount.value = ""
+    expense.value = ""
+    category.value = ""
+}
+
+
+
+
+
+
+
+
+
+amount.oninput = ()=> {
+    let value = amount.value.replace(/\D/g, "")
+
+    value = Number(value) / 100
+
+    amount.value = formatCurrencyBRL(value)
+}
+
+function formatCurrencyBRL(value) {
+
+    value = value.toLocaleString("pt-BR", {
+        style: "currency",
+        currency: "BRL"
+    })
+
+    return value
+
+}
