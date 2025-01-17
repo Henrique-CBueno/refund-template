a31eeec708703020cb9ea10f0b4b961ac2fdc598
Henrique-CBueno <henriquecb2507@outlook.pt>
2025-01-16 22:57:06 -0300

    Implement expense addition and total calculation functionality

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ const expense = document.getElementById("expense")
 const category = document.getElementById("category")
 const form = document.querySelector("form")
 
+const expenseList = document.querySelector("ul")
+const expenseQuantity = document.querySelector("aside header p span")
+const expenseTotal = document.querySelector("aside header h2")
 
 
 
@@ -21,7 +24,7 @@ form.onsubmit = (event)=>{
         currate_at: new Date(),
     }
 
-
+    expenseAdd(newExpense)
 
     console.log(newExpense)
     amount.value = ""
@@ -33,6 +36,48 @@ form.onsubmit = (event)=>{
 
 
 
+function expenseAdd(newExpense){
+    try{
+        const expenseItem = document.createElement("li")
+        expenseItem.classList.add("expense")
+
+        const expenseIcon = document.createElement("img")
+        expenseIcon.setAttribute("src", `img/${newExpense.category_id}.svg`)
+        expenseIcon.setAttribute("alt", `${newExpense.category_name}`)
+
+        const expenseInfo = document.createElement("div")
+        expenseInfo.classList.add("expense-info")
+
+        const expenseName = document.createElement("strong")
+        expenseName.textContent = newExpense.expense
+
+        const expenseCategory = document.createElement("span")
+        expenseCategory.textContent = newExpense.category_name
+
+        expenseInfo.append(expenseName, expenseCategory)
+
+        const expenseAmount = document.createElement("span")
+        expenseAmount.classList.add("expense-amount")
+        expenseAmount.innerHTML = `<small>R$</small>${newExpense.amount.toUpperCase().replace("R$", "")}`
+
+        const expenseDelete = document.createElement("img")
+        expenseDelete.classList.add("remove-icon")
+        expenseDelete.setAttribute("src", `img/remove.svg`)
+        expenseIcon.setAttribute("alt", `remove button`)
+
+        expenseItem.append(expenseIcon, expenseInfo, expenseAmount, expenseDelete)
+
+        expenseList.append(expenseItem)
+
+
+        updateTotals()
+
+
+    }catch(error){
+        alert("Não foi possivel adicionar a lista de despesas")
+        console.log(error)
+    }
+}
 
 
 
@@ -54,4 +99,45 @@ function formatCurrencyBRL(value) {
 
     return value
 
+}
+
+
+
+function updateTotals(){
+    try{
+        const itens = expenseList.children
+
+        expenseQuantity.textContent = `${itens.length} ${
+            itens.length > 1  ? "Despesas" : "Despesa"
+        }`
+
+
+        let total = 0
+
+        for(let item = 0; item < itens.length; item++){
+            const itemAmount = itens[item].querySelector(".expense-amount")
+            
+            let value = itemAmount.textContent.replace(/[^\d]/g, "").replace(",", ".")
+            value = parseFloat(value)
+
+            if(isNaN(value)){
+                return alert("nao foi possivel retornar o total")
+            }
+            
+            total += Number(value)
+        }
+
+        const symbolBRL = document.createElement("small")
+        symbolBRL.textContent = "R$"
+
+        total = formatCurrencyBRL(total / 100).toUpperCase().replace("R$", "")
+
+
+        expenseTotal.innerHTML = ""
+
+        expenseTotal.append(symbolBRL, total)
+    }catch(error){
+        alert("Não foi possivel alterar o total")
+        console.log(error)
+    }
 }
